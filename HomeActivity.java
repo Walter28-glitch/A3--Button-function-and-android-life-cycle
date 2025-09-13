d7bbc68c2954a4460fe9ff3bee7f865bd0a207b6
Walter28-glitch <robwalter.gemeniano@hcdc.edu.ph>
2025-09-13 13:30:02 +0800

    HomeActivity.java

--- HomeActivity.java
+++ HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.walter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HomeActivity extends AppCompatActivity {
 
     private EditText searchEditText;
+    private Button profileButton;
+    private Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,8 @@ public class HomeActivity extends AppCompatActivity {
 
 
         searchEditText = findViewById(R.id.etSearch);
+        profileButton = findViewById(R.id.btnProfile);
+        settingsButton = findViewById(R.id.btnSettings);
 
 
         searchEditText.setOnEditorActionListener((v, actionId, event) -> {
@@ -24,11 +30,27 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(HomeActivity.this,
                         "Searching for: " + searchQuery,
                         Toast.LENGTH_SHORT).show();
-
             }
             return true;
         });
 
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "Profile button clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "Settings button clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         Toast.makeText(this, "Welcome to TechPulse!", Toast.LENGTH_SHORT).show();
     }
